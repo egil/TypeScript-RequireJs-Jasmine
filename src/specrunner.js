08bd0be368aee5751d44b8db4525c9c083abb551
Egil Hansen <egil@assimilated.dk>
2013-02-19 16:41:01 +0100

    Added Web.config with static caching turned off
    With static caching turned off, running unit tests becomes much easier
    as files are forced to be redownloaded everytime they change.

--- src/specrunner.js
+++ src/specrunner.js
@@ -1,8 +1,7 @@
 /// <reference path=".typings/jasmine.d.ts" />
 /// <reference path=".typings/require.d.ts" />
 require.config({
-    baseUrl: 'src',
-    urlArgs: "bust=" + (new Date()).getTime()
+    baseUrl: 'src'
 });
 require([
     'spec/init'
